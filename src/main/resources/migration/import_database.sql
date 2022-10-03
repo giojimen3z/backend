8c839b592ae3e6690fdb1543fced51ae3261bbcf
jaime.jimenez <jaime.jimenez@mercadolibre.com.co>
2022-10-02 19:58:24 -0500

    Adding migrations

--- src/main/resources/migration/import_database.sql
+++ src/main/resources/migration/import_database.sql
@@ -1,1 +1,10 @@
 create schema taller;
+create table taller.articulo
+(
+    id          INTEGER     not null,
+    nombre      varchar(50) null,
+    codigo      INTEGER     null,
+    precio      FLOAT       null,
+    descripcion VARCHAR(50) null
+);
+
